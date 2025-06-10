18079e8e5a9beb46ea109e8eb00b917825051279
kivi0001 <kivi0001@stud.kea.dk>
2025-06-10 14:41:55 +0200

    Add files via upload

---
+++ tema6/portfoliosite/js/menu.js
@@ -0,0 +1,16 @@
+// BURGER MENU //
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".navi");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", burgerClick);
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+menu.addEventListener("click", menuClick);
+function menuClick() {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+}
